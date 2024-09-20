65ec3fd9eca89c67faff44f431d968759c2740aa
makaronsaf <123wert123wer7@gmail.com>
2024-09-20 19:39:11 +0300

    Add files via upload

---
+++ firstcodeonjava.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("hello world");
+        int a = scanner.nextInt() ;
+        System.out.println(a);
+    }
+}
